5b1f3ba18c3e263c96087bc2b393cbb5a8f9ca72
Bharani M <bharani91@gmail.com>
2012-01-26 06:09:10 -0800

    changes

--- config/application.rb
+++ config/application.rb
@@ -44,6 +44,5 @@ module Portfolium
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
-    config.assets.paths << "#{Rails.root}/app/assets/fonts"
   end
 end
