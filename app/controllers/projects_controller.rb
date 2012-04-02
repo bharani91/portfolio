bddb0dd016b79648f3926c6f121287d719c7f91c
Bharani M <bharani91@gmail.com>
2012-04-02 22:56:06 +0530

    changing order of posts

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -2,7 +2,7 @@ class ProjectsController < ApplicationController
   # GET /projects
   # GET /projects.json
   def index
-    @projects = Project.all
+    @projects = Project.find(:all, order: "created_at DESC")
     @tags = Project.tag_counts
     respond_to do |format|
       format.html # index.html.erb
